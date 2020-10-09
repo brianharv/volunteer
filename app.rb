72c27c4155dca7b9b09d57012210d705e0239497
Brian Harvey <brian.harv3y@gmail.com>
2020-10-09 14:07:08 -0700

    add basic CRUD functions for projects in app.rb

--- app.rb
+++ app.rb
@@ -21,6 +21,36 @@ end
 
 get('/projects/:id') do
   @project = Project.find(params[:id]).to_i
-  
+  @project.volunteers
+  erb(:project)
 end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  title = params[:project_title] 
+  @project.update({:title => title})
+  redirect to('/projects/#{params[:id].to_i()}')
+end
+
+post('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  title = params[:project_title]
+  volunteer_name = params[:volunteer_name]
+  @project.update({:title = title})
+  @volunteers = @project.volunteers
+  erb(:project)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  project.delete
+  redirect to('/projects')
+end
+
+
+
 
