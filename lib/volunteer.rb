8e01a9b52c30e3044f071ac82599df890017407e
Brian Harvey <brian.harv3y@gmail.com>
2020-10-09 11:17:17 -0700

    write code for volunteer find method

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -28,9 +28,13 @@ class Volunteer
     end
   end 
     
-  # def self.find
-    
-  # end
+  def self.find
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    name = volunteer.fetch('name')
+    project_id = volunteer.fetch('project_id').to_i
+    id = volunteer.fetch('id').to_i
+    Volunteer.new({:name => name, :project_id => project_id, :id => id})
+  end
   
   # def self.clear
    
