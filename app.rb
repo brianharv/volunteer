a69baa85e47ad22bd0a9c55f55b43da9498cc577
Brian Harvey <brian.harv3y@gmail.com>
2020-10-09 13:27:18 -0700

    add layout and projects.erb with code

--- app.rb
+++ app.rb
@@ -1,2 +1,21 @@
-require ('./app')
-run Sinatra::Application
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('./lib/volunteer')
+require('pry')
+require('pg')
+
+DB = PG.({dbname => "volunteer_tracker"})
+
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @projects = Project.all
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
