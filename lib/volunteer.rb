7e5c7360aac3e102a3ac360162ffd7edf7a4ec8f
Brian Harvey <brian.harv3y@gmail.com>
2020-10-09 10:55:32 -0700

    reconstruct proper databases.:

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,12 +1,11 @@
 class Volunteer
   attr_reader :id
-  attr_accessor :name
+  attr_accessor :name, :project_id
 
   def initialize(attributes) 
     @id = attributes.fetch(:id)
     @name = attributes.fetch(:name)
     @project_id = attributes.fetch(:project_id)
-
   end
 
   def self.all
